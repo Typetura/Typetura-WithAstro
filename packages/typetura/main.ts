6b094a8623c51625e0ba65b7f257cc69e79b2206
Peter Singh <afuzzybear@outlook.com>
2022-04-13 19:28:33 +0000

    minor nits

--- packages/typetura/main.ts
+++ packages/typetura/main.ts
@@ -1,3 +1,2 @@
-// Write your component's code here!
 export { default as Typetura } from './Typetura.astro';
-export { default as TypeturaP } from './TypeturaPro.astro';
+export { default as TypeturaP } from './TypeturaP.astro';

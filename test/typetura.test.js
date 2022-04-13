a21e31e330808a45f9a4751deb0decd9a5eacf17
Peter Singh <afuzzybear@outlook.com>
2022-04-13 19:25:40 +0000

    clean up

--- test/typetura.test.js
+++ test/typetura.test.js
@@ -2,21 +2,19 @@ import { expect } from 'chai';
 import { getComponentOutput } from 'astro-component-tester';
 
 describe('Example Tests', () => {
-	// Simple test to get us started with the syntax
+
 	it('should equal 2', () => {
 		expect(1 + 1).to.equal(2);
 	});
 
-	// This show us how to write a test for our component's output using astro-component-tester
 	describe('Component test', async () => {
 		let component;
 
-		// First get the component's output, this returns an object containing the generated html (`.html`)
 		before(async () => {
 			component = await getComponentOutput('./packages/typetura/Typetura.astro');
 		});
-		// Unless you modified /src/Component.astro, this should pass, as the component is empty apart from the frontmatter
-		it('just saying hey', () => {
+
+		it('', () => {
 			expect(component.html).to.contain('');
 		});
 	});

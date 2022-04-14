4b804b57ee9217a139260a00e40826c7098f011a
Peter Singh <afuzzybear@outlook.com>
2022-04-14 18:35:14 +0000

    Type definitions

---
+++ packages/typetura/types.d.ts
@@ -0,0 +1,34 @@
+export declare type Packages =
+	| 'Armonk'
+	| 'Austin'
+	| 'Boston'
+	| 'Brussels'
+	| 'Cape Canaveral'
+	| 'Chapel Hill'
+	| 'Chicago'
+	| 'District of Columbia'
+	| 'Fargo'
+	| 'Flagstaff'
+	| 'Kampala'
+	| 'Los Angeles'
+	| 'Madrid'
+	| 'Memphis'
+	| 'New Haven'
+	| 'New York'
+	| 'Oxford'
+	| 'Palo Alto'
+	| 'Prague'
+	| 'Saint Denis'
+	| 'Santa Fe'
+	| 'Stockholm';
+export interface PackageProps {
+	pkg: Packages;
+}
+export interface TypeturaProps {
+	cdn?: boolean;
+	base?: number;
+	scale?: number;
+	js?: boolean;
+	selectors?: string | string[];
+	pkg?: PackageProps;
+}

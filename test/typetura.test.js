d0da3f2fac81bf03f77b75d7907fea51d9177c15
Peter Singh <afuzzybear@outlook.com>
2022-04-27 20:04:25 +0000

    🛠 Fixed errors in test

--- test/typetura.test.js
+++ test/typetura.test.js
@@ -1,18 +1,9 @@
-import { expect, should } from 'chai';
+import { expect } from 'chai';
 import { getComponentOutput } from 'astro-component-tester';
-import { error } from 'console';
 
-const exectute = (fn) => {
-	return {
-		withArgs: function () {
-			const args = Array.prototype.slice.call(arguments);
-			fn(args);
-		},
-	};
-};
 const getComponent = async (props) => {
 	try {
-		return await getComponentOutput('./packages/typetura/Typetura.astro', { ...props });
+		return await getComponentOutput('./packages/typetura/components/Typetura.astro', { ...props });
 	} catch (error) {
 		throw new Error(error);
 	}
@@ -23,68 +14,65 @@ describe('Test: <Typetura/> Component', () => {
 
 	describe('Output Primary render test:\n ', () => {
 		before(async () => {
-			component = await getComponentOutput('./packages/typetura/Typetura.astro');
+			component = await getComponent();
 		});
 		it("Render a <script src='cdn'> && <style> with the default base and style applied</style>", () => {
-			expect(component.html).to.equal(
-				'<head><script src="https://cdn.jsdelivr.net/gh/scottkellum/typetura.js@master/js/typetura.min.js"></script>\n' +
-					'\t<style>\n' +
-					'\t:root{\n' +
-					'\t\t--tt-base:20;\n' +
-					'\t\t--tt-scale:1;\n' +
-					'\t}\n' +
-					'\t</style>\n' +
-					'</head><body></body>'
-			);
+			expect(component.html)
+				.to.include(`script`)
+				.and.to.include(`src="https://cdn.jsdelivr.net/gh/scottkellum/typetura.js@master/js/typetura.min.js"`)
+				.and.to.include('style')
+				.and.to.include(`:root`)
+				.and.to.include(`--tt-base:20;`)
+				.and.to.include(`--tt-scale:1;`);
 		});
 	});
 
 	describe('Render with JS Output', () => {
 		before(async () => {
-			component = await getComponentOutput('./packages/typetura/Typetura.astro', { js: true });
+			component = await getComponent({ js: true });
 		});
-		it(``, () => {
-			expect(component.html).to.include('window.typetura');
+		it(`Render : <script>window.typetura = {}</script>`, () => {
+			expect(component.html).to.include('script').and.to.include('window.typetura');
 		});
 	});
-	describe('Test Prop : <Typetura selectors/>', () => {
-		describe('Single Prop Value', () => {
+	describe('Test : Prop.selectors', () => {
+		describe('Single Selector', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { js: true, selectors: '.test' });
+				component = await getComponent({ js: true, selectors: '.test' });
 			});
-			it(`Render selectors output `, () => {
-				expect(component.html).to.include(`selectors:[.test]`);
+			it(`Render : selector:['.test'] `, () => {
+				expect(component.html).to.include(`selectors:['.test']`);
 			});
 		});
 		describe('Multiple Prop Values', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { js: true, selectors: '.test,test' });
+				component = await getComponent({ js: true, selectors: '.test,test' });
 			});
-			it(`Render selectors output `, () => {
-				expect(component.html).to.include(`selectors:[.test,test]`);
+			it(`Render : selectors:['.test','test'] `, () => {
+				expect(component.html).to.include(`selectors:['.test','test']`);
 			});
 		});
 	});
 	describe('Test Prop: <Typetura base/>', () => {
 		describe('Check Default Prop  Value in CSS', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro');
+				component = await getComponent();
 			});
-			it(`Must return :root && --tt-base:20`, () => {
+			it(`Render : :root {--tt-base:20}`, () => {
 				expect(component.html).to.include(':root').and.to.include(`--tt-base:20`);
 			});
 		});
 		describe('Check Default Prop Value in JS ', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { js: true });
+				component = await getComponent({ js: true });
 			});
-			it(`Must return window && base:20 `, () => {
-				expect(component.html).to.include(`window.typetura`).and.to.include('base:20');
+			it(`Render: window.typetura={base:20} `, () => {
+				expect(component.html).to.include(`window.typetura`).and.to.include('base: 20');
 			});
 		});
 		describe('Alternative Base Value in CSS', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { base: 10 });
+				component = await getComponent({ base: 10 });
 			});
 			it(`Reflects an alternate Base Value of 10 `, () => {
 				expect(component.html).to.include(':root').and.to.include(`base:10`);
@@ -92,17 +80,17 @@ describe('Test: <Typetura/> Component', () => {
 		});
 		describe('Alternative Base Value in JS', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { js: true, base: 10 });
+				component = await getComponent({ js: true, base: 10 });
 			});
 			it(`Reflects an alternate Base Value of 10 `, () => {
-				expect(component.html).to.include('window.typetura').and.to.include(`base:10`);
+				expect(component.html).to.include('window.typetura').and.to.include(`base: 10`);
 			});
 		});
 	});
 	describe('Test Prop: <Typetura scale/>', () => {
 		describe('Check Default Prop  Value in CSS', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro');
+				component = await getComponent();
 			});
 			it(`Must return :root && --tt-scale:1`, () => {
 				expect(component.html).to.include(':root').and.to.include(`--tt-scale:1`);
@@ -110,26 +98,26 @@ describe('Test: <Typetura/> Component', () => {
 		});
 		describe('Check Default Prop Value in JS ', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { js: true });
+				component = await getComponent({ js: true });
 			});
 			it(`Must return window && scale:1`, () => {
-				expect(component.html).to.include(`window.typetura`).and.to.include('scale:1');
+				expect(component.html).to.include(`window.typetura`).and.to.include('scale: 1');
 			});
 		});
 		describe('Alternative Scale Value in CSS', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { scale: 0.5 });
+				component = await getComponent({ scale: 0.5 });
 			});
 			it(`Reflects an alternate Scale Value of 0.5 `, () => {
-				expect(component.html).to.include(':root').and.to.include(`scale:0.5`);
+				expect(component.html).to.include(':root').and.to.include(`--tt-scale:0.5`);
 			});
 		});
 		describe('Alternative Scale Value in 0.5', () => {
 			before(async () => {
-				component = await getComponentOutput('./packages/typetura/Typetura.astro', { js: true, scale: 0.5 });
+				component = await getComponent({ js: true, scale: 0.5 });
 			});
 			it(`Reflects an alternate Scale Value of 0.5 `, () => {
-				expect(component.html).to.include('window.typetura').and.to.include(`scale:0.5`);
+				expect(component.html).to.include('window.typetura').and.to.include(`scale: 0.5`);
 			});
 		});
 	});

4b5e94d8c7f25d531e508f61da0a9c87211d301a
Peter Singh <afuzzybear@outlook.com>
2022-05-04 12:47:15 +0000

    🍒 Updated Test

---
+++ test/section-headline.test.js
@@ -0,0 +1,160 @@
+import { expect } from 'chai';
+import { getComponentOutput } from 'astro-component-tester';
+
+const getComponent = async (props) => {
+	try {
+		return await (
+			await getComponentOutput('./packages/typetura/components/SectionHeadline.astro', { ...props })
+		).html;
+	} catch (error) {
+		return error;
+	}
+};
+
+describe('Test : <SectionHeadline/>', () => {
+	describe('Test range of <SectionHeadline/> Components', () => {
+		let component;
+		describe('Check Default Output', () => {
+			before(async () => {
+				component = await getComponent();
+			});
+			it('Render Default <h2>', () => {
+				expect(component).to.include('h2');
+			});
+		});
+		describe('level = 1', () => {
+			before(async () => {
+				component = await getComponent({ level: 1 });
+			});
+			it('Render Default <h1>', () => {
+				expect(component).to.include('h1');
+			});
+		});
+		describe('level = "h1"', () => {
+			before(async () => {
+				component = await getComponent({ level: 'h1' });
+			});
+			it('Render Default <h1>', () => {
+				expect(component).to.include('h1');
+			});
+		});
+		describe('level = 3', () => {
+			before(async () => {
+				component = await getComponent({ level: 3 });
+			});
+			it('Render <h3>', () => {
+				expect(component).to.include('h3');
+			});
+		});
+		describe('level = "h3"', () => {
+			before(async () => {
+				component = await getComponent({ level: 'h3' });
+			});
+			it('Render <h3>', () => {
+				expect(component).to.include('h3');
+			});
+		});
+		describe('level = 4', () => {
+			before(async () => {
+				component = await getComponent({ level: 4 });
+			});
+			it('Render <h4>', () => {
+				expect(component).to.include('h4');
+			});
+		});
+		describe('level = "h4"', () => {
+			before(async () => {
+				component = await getComponent({ level: 'h4' });
+			});
+			it('Render <h4>', () => {
+				expect(component).to.include('h4');
+			});
+		});
+		describe('level = 5', () => {
+			before(async () => {
+				component = await getComponent({ level: 5 });
+			});
+			it('Render <h5>', () => {
+				expect(component).to.include('h5');
+			});
+		});
+		describe('level = "h5"', () => {
+			before(async () => {
+				component = await getComponent({ level: 'h5' });
+			});
+			it('Render <h5>', () => {
+				expect(component).to.include('h5');
+			});
+		});
+	});
+
+	describe('Test : Props.none', () => {
+		let component;
+		before(async () => {
+			component = await getComponent({ none: true });
+		});
+		it('Render class="section-headline"', () => {
+			expect(component).to.include('class').and.to.include('section-headline').and.to.include('--tt-key:none;');
+		});
+	});
+
+	describe('Test: Prop.base', () => {
+		let component;
+		before(async () => {
+			component = await getComponent({ base: 42 });
+		});
+		it('Render: <style>.section-headline {--tt-base:42;}', () => {
+			expect(component).to.include('style').and.to.include('.section-headline').and.to.include('--tt-base:42');
+		});
+	});
+
+	describe('Test: Prop.scale', () => {
+		let component;
+		before(async () => {
+			component = await getComponent({ scale: 0.5 });
+		});
+		it('Render: <style>.section-headline {--tt-scale:0.5;}', () => {
+			expect(component).to.include('style').and.to.include('.section-headline').and.to.include('--tt-scale:0.5');
+		});
+	});
+
+	describe('Test: Prop.ease', () => {
+		let component;
+		before(async () => {
+			component = await getComponent({ ease: 'ease-in' });
+		});
+		it('Render: <style>.section-headline {--tt-ease:ease-in;}', () => {
+			expect(component).to.include('style').and.to.include('.section-headline').and.to.include('--tt-ease:ease-in');
+		});
+	});
+
+	describe('Test: Prop.key', () => {
+		let component;
+		before(async () => {
+			component = await getComponent({ key: 'test' });
+		});
+		it('Render: <style>.section-headline {--tt-key:test;}', () => {
+			expect(component).to.include('style').and.to.include('.section-headline').and.to.include('--tt-key:test;');
+		});
+	});
+
+	describe('👓 <SectionHeadline key="special" base={42} scale={0.5} ease=\'ease-in\' id="test"/>', () => {
+		let component;
+		before(async () => {
+			component = await getComponent({ level: 'h3', key: 'special', ease: 'ease-in', base: 42, scale: 0.5, id: 'test', class: 'flex red' });
+		});
+		it('Render: `<style>.section-headline-**UUID** {--tt-key:special;--tt-ease:ease-in;--tt-base:42;--tt-scale:0.5;}</style><h2 class="primary-headline-**UUID** flex red"/>', () => {
+			expect(component)
+				.to.include('style')
+				.and.to.include('section-headline')
+				.and.to.include('--tt-key:special;')
+				.and.to.include('--tt-ease:ease-in;')
+				.and.to.include('--tt-base:42;')
+				.and.to.include('--tt-scale:0.5;')
+				.and.to.include('h3')
+				.and.to.include('class')
+				.and.to.include(`flex red"`)
+				.and.to.include('id="test"');
+		});
+	});
+});
